c74f65f06e426cff01aa5483010d12dd6b736a7b
Isaac <isaacluiz0808@gmail.com>
2025-04-04 23:31:45 -0300

    Finalizando projeto

---
+++ src/main/resources/db/changelog/migrations/db.changelog-202504042326.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+--changeset isaac:202504042326
+--comment: boards table create
+
+CREATE TABLE BOARDS(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+) ENGINE=InnoDB;
+
+--rollback DROP TABLE BOARDS
